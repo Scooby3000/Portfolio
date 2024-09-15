35750335d808ae4759a53eb104337eafd7e931fd
Dhairya Shah <dhairyashah3000@gmail.com>
2024-09-15 18:53:40 -0400

    udated js

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,6 +13,7 @@
 
         if ($(this).parents('.nav-menu, .mobile-nav').length) {
           $('.nav-menu .active, .mobile-nav .active').removeClass('active');
+          
           $(this).closest('li').addClass('active');
         }
 
